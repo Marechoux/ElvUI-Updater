b114ec75fd60d56da83c5f64df3c7a119906b27e
Nicolas M <me@marechoux.com>
2024-05-31 17:00:02 +0200

    update assembly infos

--- ElvUI Updater/Properties/AssemblyInfo.cs
+++ ElvUI Updater/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nico - Marechoux.com")]
 [assembly: AssemblyProduct("ElvUI Updater")]
-[assembly: AssemblyCopyright("Copyright © Marechoux.com 2019")]
+[assembly: AssemblyCopyright("Copyright © Choux.io 2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.1.0")]
-[assembly: AssemblyFileVersion("0.1.1.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
